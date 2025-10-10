dc9265b9a02165f6b7b2d3746464d7ef564a4b3d
Sándor Norbert <sandor.norbert@ckik.loc>
2025-10-10 09:24:09 +0200

    hazi

--- hazi 3.py
+++ hazi 3.py
@@ -27,34 +27,4 @@ if a == b and b == c:
 else:
     print("Nem szabályos háromszög")
 
-szam1 = int(input("Adjon meg egy egész számot: "))
-
-if szam1 % 2 == 0:
-    print("páros")
-else:
-    print("páratlan")
-
-szam2 = int(input("Adjon meg egy 2. egész számot: "))
-
-if szam2 % 10 == 0:
-    print("tízzel osztható")
-else:
-    print("tízzel nem osztható, utolsó számjegy:", szam2 % 10)
-
-szam3 = int(input("Adjon meg egy 3. egész számot is: "))
-
-if szam2 != 0 and szam3 != 0:
-    rec2 = 1 / szam2
-    rec3 = 1 / szam3
-    print("A két szám reciprokának összege:", rec2 + rec3)
-else:
-    if szam2 == 0:
-        print("A második számnak nincs reciproka (0)")
-    if szam3 == 0:
-        print("A harmadik számnak nincs reciproka (0)")
-
-osszeg = szam2 + szam3
-if osszeg >= 0:
-    print("A két szám összegének gyöke:", math.sqrt(osszeg))
-else:
-    print("A két szám összege negatív, nincs valós gyöke.")
+# HF: python - ciklusok, loop, interációk,...
