a9c35c0f19d25cd6d0fc28a3b0322553788bc033
Sándor Norbert <sandor.norbert@ckik.loc>
2025-10-16 10:35:24 +0200

    hazi

--- szovegkezeles.py
+++ szovegkezeles.py
@@ -1,7 +1,43 @@
-# lebegőpontos - float - tört
-a = 1.25
-b = float(input("adjon meg egy tizedes törtet: "))
-print(b*4)
+import random
 
-# generáljon ki [1,9] közötti tört számot 2 tizedesjegyre
+
+# # lebegőpontos - float - tört
+# a = 1.25
+# b = float(input("adjon meg egy tizedes törtet: "))
+# print(b*4)
+
+# generáljon ki [1,10[ közötti tört számot 2 tizedesjegyre
 # pl. 1.36, 2.30
+
+# c = random.randint(100,999)/100
+# c = random.random() #[0,1˙[
+# print(round(c,2))   #HF befejezni
+
+# Szövegkezelés
+# szoveg = input("adjon meg egy szöveget: ")
+# print(szoveg)
+# print("Szöveg hossza: ", len(szoveg))
+# print("első karakter" ,szoveg[0])
+# # szöveg karakterekből épül fel
+# # szöveg = karakter lánc
+# karakter = szoveg[0]
+# kod = ord(szoveg[0])
+# print(kod)
+# ujkod = kod + 1
+# ujkarakter = chr(ujkod)
+# print(ujkarakter)
+
+# a = chr(random.randint(97,122))
+# b = chr(random.randint(97,122))
+# c = chr(random.randint(97,122))
+# print(a,b,c,)
+
+
+
+# Kérje be a felhasználó keresztnevét! Generáljon neki egy jelszót, az első 3 karakterének ascii kódjának szorzatát! Ha nincs a név 3 jegyű, akkor kettő esetén a hossz érték legyen a szorzat3. tagja 1 esetén pedig a  szám köbe leygyen
+# Alma - 65 * 108 * 109
+# Co - 67 * 111 * 2
+#G - 71 * 71 * 71
+
+szoveg = input("Adja meg a keresztnevét: ")
+print(szoveg)
