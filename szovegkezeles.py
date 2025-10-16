498bfc851fdc680c343b7f5a09d90e052a7c45ea
Sándor Norbert <sandor.norbert@ckik.loc>
2025-10-16 09:40:10 +0200

    holnap doga gatya

---
+++ szovegkezeles.py
@@ -0,0 +1,7 @@
+# lebegőpontos - float - tört
+a = 1.25
+b = float(input("adjon meg egy tizedes törtet: "))
+print(b*4)
+
+# generáljon ki [1,9] közötti tört számot 2 tizedesjegyre
+# pl. 1.36, 2.30
