9b8068d18f550cacdaa482a87f3f4674446244bd
Norbi <sandor.norbert@ckik.hu>
2025-11-26 20:16:29 +0100

    idk

--- 2.doga javitas.py
+++ 2.doga javitas.py
@@ -1,33 +1,35 @@
-import random 
+import random
 import math
 
 # 1.a
-velszam = random.randint(10,99)
+velszam = random.randint(10, 99)
 print(velszam)
-
 
 # 1.b
 db = 0
-for haromjegyu in range(0,velszam,1):
-    haromjegyu = random.randint(100,999)
-    if(haromjegyu % 3 == 0):
-        db+=1
-    print(haromjegyu,end=" ")
+osszeg = 0
+for szamlalo in range(velszam):
+    haromjegyu = random.randint(100, 999)
+    if haromjegyu % 3 == 0:
+        db += 1
+    osszeg += haromjegyu
+    print(haromjegyu, end=" ")
+
 print()
 print(db)
 print(round(math.sqrt(osszeg), 2))
 
-
-# 2.feladat
+# 2. feladat
 szoveg = input("Adjon meg egy szöveget: ")
-len(szoveg) = hossz
-if( hossz % 2 == 0):
-    index = hossz//2
+hossz = len(szoveg)
+
+if hossz % 2 == 1:  # páratlan hossz → van középső
+    index = hossz // 2
     print(szoveg[index])
-else:
-    index1 = hossz//2
-    index2 = hossz//2-1
+else:               # páros hossz → két középső
+    index1 = hossz // 2 - 1
+    index2 = hossz // 2
     print(szoveg[index1], szoveg[index2])
 
-for ix in range(0,hossz,2):
-print(szoveg[ix],end="$")
+for ix in range(0, hossz, 2):
+    print(szoveg[ix], end="$")
